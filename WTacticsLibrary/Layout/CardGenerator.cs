8f7e0acebcd7730e49d41cb1a1b0b657bd343e56
Nico Goeminne <nico.goeminne@metanous.be>
2016-04-16 11:18:32 +0200

    - changed back to rsvg gexporter

--- WTacticsLibrary/Layout/CardGenerator.cs
+++ WTacticsLibrary/Layout/CardGenerator.cs
@@ -30,7 +30,7 @@ namespace WTacticsLibrary.Layout
         public static void CreatePngJob(Guid cardGuid)
         {
             var svgFile = Repository.GetSvgFile(cardGuid);
-            InkscapeExporter.ExportPng(svgFile, Repository.GetHighResolutionPngFile(cardGuid));
+            RsvgExporter.ExportPng(svgFile, Repository.GetHighResolutionPngFile(cardGuid));
             using (var repository = new Repository())
             {
                 var cardModel = repository.Context.Cards.FindByGuid(cardGuid);
