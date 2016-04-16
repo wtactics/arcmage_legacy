1abc4500fb1b9a2f4a9abec3c560eecf42e943bf
Nico Goeminne <nico.goeminne@metanous.be>
2016-04-16 23:52:40 +0200

    - added impersonation

---
+++ WTacticsLibrary/Layout/ImpersonateUserProcess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WTacticsLibrary.Layout
+{
+    public class ImpersonateUserProcess
+    {
+        public static void Impersonate(Process process, string username, string password)
+        {
+            System.Security.SecureString ssPwd = new System.Security.SecureString();
+            process.StartInfo.UserName = username;
+            for (int x = 0; x < password.Length; x++)
+            {
+                ssPwd.AppendChar(password[x]);
+            }
+            process.StartInfo.Password = ssPwd;
+        }
+    }
+}
--- WTacticsLibrary/Layout/InkscapeExporter.cs
+++ WTacticsLibrary/Layout/InkscapeExporter.cs
@@ -62,5 +62,7 @@ namespace WTacticsLibrary.Layout
             }
         }
 
+      
+
     }
 }
